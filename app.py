9e5723bdf0ed796d24b255e9f426a1c4b1d5f294
Corey <66322946+soupsamich@users.noreply.github.com>
2025-09-05 03:47:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/static/poc/malicious.txt')
-def serve_malicious():
-    response = make_response(send_from_directory('static/poc', 'malicious.txt'))
-    if request.args.get('nosniff') == 'true':
-        response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
 if __name__ == '__main__':
     app.run(debug=True)
